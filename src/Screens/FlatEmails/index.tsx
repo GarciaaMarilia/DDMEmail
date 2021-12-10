4dc75f4593958d93923f84a2a2d0234e40b80e78
GarciaaMarilia <garciaamarilia@gmail.com>
2021-12-10 13:03:16 -0300

    Screen FlatList de E-mails

---
+++ src/Screens/FlatEmails/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    FlatList,
+    ScrollView
+} from 'react-native';
+
+export function FlatEmails() {
+
+    const emails = [
+        id: string;
+        to: string;
+        tittle: string;
+        summary: string;
+        time: string;
+        star: boolean;
+        picture: URL;
+    ]
+
+    return (
+        <View>
+            <ScrollView>
+                <FlatList>
+
+                </FlatList>
+            </ScrollView>
+        </View>
+    )
+}
